c743d7b6f5636631234b63079504dd6723340d55
GyeongHo Kim <rlarudgh2017@gmail.com>
2025-04-27 18:25:13 +0900

    chore: check emsdk folder before clone it

--- build.sh
+++ build.sh
@@ -11,13 +11,24 @@ fi
 
 echo "Check emcc already installed"
 if ! command -v emcc --version > /dev/null 2>&1; then
-    echo "emcc not found, install emscripten"
-    git clone https://github.com/emscripten-core/emsdk.git
-    cd emsdk
-    ./emsdk install 4.0.5
-    ./emsdk activate 4.0.5
-    source ./emsdk_env.sh
-    cd ..
+    echo "emcc not found, check emsdk already exists"
+    if [ -d "emsdk" ]; then
+        echo "emsdk already exists, set environment variables"
+        cd emsdk
+        git pull
+        ./emsdk install 4.0.5
+        ./emsdk activate 4.0.5
+        source ./emsdk_env.sh
+        cd ..
+    else
+        echo "emsdk not found, clone and install emscripten"
+        git clone https://github.com/emscripten-core/emsdk.git
+        cd emsdk
+        ./emsdk install 4.0.5
+        ./emsdk activate 4.0.5
+        source ./emsdk_env.sh
+        cd ..
+    fi
 fi
 
 ./build_hevc_decoder.sh
